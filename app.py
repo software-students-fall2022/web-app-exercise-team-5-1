f2b1f8ae672977ee77b4c9dc11c75cb901765952
bxw201 <bxw201@nyu.edu>
2022-10-17 02:54:10 -0400

    bug fix: fixed requirements and func mapping

--- app.py
+++ app.py
@@ -24,14 +24,14 @@ def listing_ask(id):
     return render_template('listing_ask.html')
 
 @app.route('/listing/<id>/answer/<qid>')
-def listing_ask(id, qid):
+def listing_answer(id, qid):
     return render_template('listing_answer.html')
 
 #test to insert data to the data base
 @app.route("/api/db_test")
 def test():
     db.collection.insert_one({"name": "John"})
-    return "Connected to the data base!"
+    return "Connected to the database!"
 
 if __name__ == '__main__':
     app.run(port=3000)
