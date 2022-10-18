20c5fce9b1dafe06ef26ec089003465082b6a6ab
Harrison Douglass <hpdouglass10@gmail.com>
2022-10-18 16:03:49 -0400

    Implement listing creation functionality

--- listing_create.py
+++ listing_create.py
@@ -1,4 +1,45 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from db import listings_collection
+import time
+import bcrypt
 
 def listing_create():
-    return render_template('listing_create.html')
+    if request.method == "POST":
+        # If request method is POST process submitted form data.
+        try:
+            # If no errors, add listing to database and redirect to new listing page.
+            listing = listing_object_from_params(request.form, request.files)
+            result = listings_collection.insert_one(listing)
+            return redirect(url_for("listing", id=str(result.inserted_id)))
+        except Exception as e:
+            # Return any raised validation errors and send 400 code.
+            return str(e), 400
+    else:
+        # Otherwise, request method was GET so load the form.
+        return render_template('listing_create.html')
+
+def listing_object_from_params(form, files):
+    # Check that all required attributes are present in the request.
+    reqs = ["title", "desc", "price", "author", "password"]
+    for req in reqs:
+        if not form.__contains__(req):
+            raise Exception(f"\"{req}\" attribute must be present.")
+
+    # Handle hashing and salting of provided password.
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(form.get("password").encode('utf8'), salt)
+    
+    # Return database-ready listing. Errors may still arise in this section.
+    return {
+        "timestamp": int(time.time()),
+        "title": form.get("title"),
+        "description": form.get("desc"),
+        "price": round(float(form.get("price")), 2),
+        "images": [], # TODO: Handle images.
+        "author": form.get("author"),
+        "password": {
+            "hash": hash,
+            "salt": salt,
+        },
+        "questions": []
+    }
