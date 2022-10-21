e3d9d985f5fd51ebed70395ceaa76819924790c8
bxw201 <bxw201@nyu.edu>
2022-10-21 12:29:47 -0400

    refactor: separate update and delete

--- listing_update.py
+++ listing_update.py
@@ -19,40 +19,25 @@ def listing_update(id):
         # Check password was submitted and correct before changing the listing in any way.
         if "password" not in request.form:
             return render_template('error.html', message = "You must submit a passcode to edit this listing."), 401
+
         if not bcrypt.checkpw(request.form.get("password").encode('utf8'), listing["password"]):
             return render_template('error.html', message = "The provided passcode is not valid for this listing."), 401
         
-        # Check value of submit button. (TODO: Maybe there is a better way to do this?)
-        # Process the form based on which action the user has taken.
-        if "action" not in request.form:
-            return render_template('error.html', message = "No action type given with form data."), 400
-        action = request.form.get("action")
-        if action == "Delete Listing":
-            # Delete the listing from the database and redirect to index.
-            listings_collection.delete_one({'_id': ObjectId(id)})
-            return redirect("/")
-        elif action == "Edit Listing":
-            # Otherwise edit the listing with the new form data.
-            return edit_listing(id, request.form, request.files)
-        else:
-            # If action is not equal to one of the predefined options, raise an error.
-            return render_template('error.html', message = "Invalid action type given with form data."), 400
+        newlistinginfo = {}
+        # Check each field and see if the form data has a value for it. If so, update the database.
+        for param in ["title", "description", "author"]:
+            if "author" in request.form:
+                newlistinginfo[param] = request.form.get(param)
+        if "price" in request.form:
+            newlistinginfo["price"] = round(float(request.form.get("price")), 2)
+
+        # Update the listing in the database and redirect to that listing page.
+        listings_collection.update_one({'_id': ObjectId(id)}, {'$set': newlistinginfo})
+        return redirect(url_for("listing", id=id))
+
     except StopIteration:
         return render_template('error.html', message = "No listing found with this id."), 404
     except InvalidId:
         return render_template('error.html', message = "The given id is invalid."), 400
     except TypeError as te:
         return render_template('error.html', message = str(te)), 400
-
-def edit_listing(id, form, files):
-    newlistinginfo = {}
-    # Check each field and see if the form data has a value for it. If so, update the database.
-    for param in ["title", "description", "author"]:
-        if "author" in form:
-            newlistinginfo[param] = form.get(param)
-    if "price" in form:
-        newlistinginfo["price"] = round(float(form.get("price")), 2)
-
-    # Update the listing in the database and redirect to that listing page.
-    listings_collection.update_one({'_id': ObjectId(id)}, {'$set': newlistinginfo})
-    return redirect(url_for("listing", id=str(id)))
